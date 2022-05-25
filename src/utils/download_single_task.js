3160081d93a5ad5bbe28fe9d0638505ffe07f5e5
shicheng.yu <1542874601@qq.com>
2022-05-25 11:07:40 +0800

    temp:临时节点标记 星期三 上午 11:07:39 五月 25日 2022 修改了 src/utils/download_single_task.js

--- src/utils/download_single_task.js
+++ src/utils/download_single_task.js
@@ -6,17 +6,20 @@ import publish_mission from "@/utils/publish_mission";
 
 const youtube_dl = path.resolve(__dirname, "../../youtube-dl.exe");
 
+const proxy_config = [];
+// const proxy_config = ["--proxy", "socks5://127.0.0.1:1080"];
+
 export default async function download_single(videoId) {
   try {
     const video_source_url = `https://www.youtube.com/watch?v=${videoId}`;
-    const stdout_title = spawn(youtube_dl, ["--get-title", video_source_url, "--proxy", "socks5://127.0.0.1:1080"]);
+    const stdout_title = spawn(youtube_dl, ["--get-title", video_source_url, ...proxy_config]);
     const video_title = await new Promise((resolve, reject) => {
       stdout_title.stdout.on("data", (data) => resolve(data));
       stdout_title.stderr.on("data", (error) => reject(error.toString()));
     });
     const decode_title = iconv_lite.decode(video_title, "cp936").replace(/\n/ig, "").replace(/\%/ig, "");
     const download_dir_path = path.resolve(__dirname, `../../statics/${decode_title} #鸡汤 #励志 .%(ext)s`);
-    const download_mission = spawn(youtube_dl, ["--format", "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best", "-o", download_dir_path, video_source_url, "--proxy", "socks5://127.0.0.1:1080"], { cwd: process.cwd() });
+    const download_mission = spawn(youtube_dl, ["--format", "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best", "-o", download_dir_path, video_source_url, ...proxy_config], { cwd: process.cwd() });
     await new Promise((resolve, reject) => {
       download_mission.on("close", resolve);
       download_mission.stdout.on("data", (data) => {
